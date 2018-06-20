59b8fc1628045c369efa41c6468f36853685605e
blue0403 <artur.lichaj@gmail.com>
2018-06-20 18:06:18 +0200

    Extend iteration over returned boards with filtering

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -21,6 +21,10 @@ public class TrelloController {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+        trelloBoards.stream()
+                .filter(i -> i.getName() != null)
+                .filter(j -> j.getId() != null)
+                .filter(k -> k.getName().toLowerCase().contains("kodilla"))
+                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
     }
 }
