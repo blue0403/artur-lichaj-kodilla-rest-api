4a47bb649b70a76cffa5c8c3adf706789b6647a9
blue0403 <artur.lichaj@gmail.com>
2018-07-24 20:14:38 +0200

    Create and implement validator

--- src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -3,6 +3,7 @@ package com.crud.tasks.trello.facade;
 import com.crud.tasks.domain.*;
 import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.validator.TrelloValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,23 +22,18 @@ public class TrelloFacade {
     @Autowired
     private TrelloMapper trelloMapper;
 
+    @Autowired
+    private TrelloValidator trelloValidator;
+
     public List<TrelloBoardDto> fetchTrelloBoards() {
         List<TrelloBoard> trelloBoards = trelloMapper.mapToBoards(trelloService.fetchTrelloBoards());
-        LOGGER.info("Starting filtering boards...");
-        List<TrelloBoard> filteredBoards = trelloBoards.stream()
-                .filter(trelloBoard -> !trelloBoard.getName().equalsIgnoreCase("test"))
-                .collect(Collectors.toList());
-        LOGGER.info("Boards have been filtered. Current list size: " + filteredBoards.size());
+        List<TrelloBoard> filteredBoards = trelloValidator.validateTrelloBoards(trelloBoards);
         return trelloMapper.mapToBoardsDto(filteredBoards);
     }
 
     public CreatedTrelloCardDto createCard(final TrelloCardDto trelloCardDto) {
         TrelloCard trelloCard = trelloMapper.mapToCard(trelloCardDto);
-        if (trelloCard.getName().contains("test")) {
-            LOGGER.info("Someone is testing my application");
-        } else {
-            LOGGER.info("Seems that my application is used in a proper way");
-        }
+        trelloValidator.validateCard(trelloCard);
         return trelloService.createTrelloCard(trelloMapper.mapToCardDto(trelloCard));
     }
 }
