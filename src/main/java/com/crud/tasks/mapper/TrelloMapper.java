ee8181d86119dd5d67eb24ec5a29f6282ce5b2a4
blue0403 <artur.lichaj@gmail.com>
2018-07-24 16:33:09 +0200

    Add mapper and classes representing domain objects

---
+++ src/main/java/com/crud/tasks/mapper/TrelloMapper.java
@@ -0,0 +1,43 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloList;
+import com.crud.tasks.domain.TrelloListDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Component
+public class TrelloMapper {
+
+    public List<TrelloBoard> mapToBoards(final List<TrelloBoardDto> trelloBoardDto) {
+        return trelloBoardDto.stream()
+                .map(trelloBoardDTO -> new TrelloBoard(trelloBoardDTO.getId(), trelloBoardDTO.getName(),
+                        mapToList(trelloBoardDTO.getLists())))
+                .collect(toList());
+    }
+
+    public List<TrelloBoardDto> mapToBoardsDto(final List<TrelloBoard> trelloBoards) {
+        return trelloBoards.stream()
+                .map(trelloBoard -> new TrelloBoardDto(trelloBoard.getId(), trelloBoard.getName(),
+                        mapToListDto(trelloBoard.getLists())))
+                .collect(toList());
+    }
+
+    public List<TrelloList> mapToList(final List<TrelloListDto> trelloListDto) {
+        return trelloListDto.stream()
+                .map(trelloListDTO -> new TrelloList(trelloListDTO.getId(), trelloListDTO.getName(),
+                        trelloListDTO.isClosed()))
+                .collect(toList());
+    }
+
+    public List<TrelloListDto> mapToListDto(final List<TrelloList> trelloLists) {
+        return trelloLists.stream()
+                .map(trelloList -> new TrelloListDto(trelloList.getId(), trelloList.getName(),
+                        trelloList.isClosed()))
+                .collect(toList());
+    }
+}
