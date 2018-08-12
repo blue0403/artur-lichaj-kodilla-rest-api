f59f0faffbe6dc564e83223c03a16a666981134a
blue0403 <artur.lichaj@gmail.com>
2018-08-12 21:22:14 +0200

    Add test shouldFetchEmptyList()

---
+++ src/test/java/com/crud/tasks/service/TrelloServiceTest.java
@@ -0,0 +1,46 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrelloServiceTest {
+
+    @InjectMocks
+    private TrelloService trelloService;
+
+    @Mock
+    private TrelloClient trelloClient;
+
+    @Test
+    public void shouldFetchEmptyList() {
+        //Given
+        List<TrelloListDto> trelloLists = new ArrayList<>();
+        trelloLists.add(new TrelloListDto("1", "test_list", false));
+
+        List<TrelloBoardDto> trelloBoards = new ArrayList<>();
+        trelloBoards.add(new TrelloBoardDto("test", "1", trelloLists));
+
+        when(trelloClient.getTrelloBoards()).thenReturn(new ArrayList<>());
+
+        //When
+        List<TrelloBoardDto> trelloBoardDtos = trelloService.fetchTrelloBoards();
+
+        //Then
+        assertNotNull(trelloBoardDtos);
+        assertEquals(0, trelloBoardDtos.size());
+    }
+}
