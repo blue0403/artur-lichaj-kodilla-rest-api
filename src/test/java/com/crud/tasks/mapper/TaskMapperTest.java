7d0d9f843048261d2df5010db2e538417b514e2b
blue0403 <artur.lichaj@gmail.com>
2018-08-12 20:37:14 +0200

    Add test shouldMapToTaskDtoList()

--- src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
@@ -7,6 +7,9 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -34,10 +37,32 @@ public class TaskMapperTest {
         Task task = new Task(1L,"test_title","test_content");
 
         //When
-        TaskDto taskDto =  taskMapper.mapToTaskDto(task);
+        TaskDto taskDto = taskMapper.mapToTaskDto(task);
 
         //Then
         assertNotNull(taskDto);
         assertEquals("test_content", taskDto.getContent());
+    }
+
+    @Test
+    public void shouldMapToTaskDtoList() {
+        //Given
+        Task task = new Task(1L,"test_title","test_content");
+
+        List<Task> taskList = new ArrayList<>();
+        taskList.add(task);
+
+        //When
+        List<TaskDto> taskDtoLists = taskMapper.mapToTaskDtoList(taskList);
+
+        //Then
+        assertNotNull(taskDtoLists);
+        assertEquals(1, taskDtoLists.size());
+
+        taskDtoLists.forEach(taskDtoList -> {
+            assertEquals(1L, taskDtoList.getId().longValue());
+            assertEquals("test_title",taskDtoList.getTitle());
+            assertEquals("test_content",taskDtoList.getContent());
+        });
     }
 }
