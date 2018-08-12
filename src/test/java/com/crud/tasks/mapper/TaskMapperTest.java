30c8d43f6aa4943f37c813db1c294604d1069486
blue0403 <artur.lichaj@gmail.com>
2018-08-12 20:03:27 +0200

    Add test shouldMapToTask()

---
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TaskMapperTest {
+
+    @InjectMocks
+    private TaskMapper taskMapper;
+
+    @Test
+    public void shouldMapToTask() {
+        //Given
+        TaskDto taskDto = new TaskDto(1L,"test_title","test_content");
+
+        //When
+        Task task = taskMapper.mapToTask(taskDto);
+
+        //Then
+        assertNotNull(task);
+        assertEquals("test_title", task.getTitle());
+    }
+}
